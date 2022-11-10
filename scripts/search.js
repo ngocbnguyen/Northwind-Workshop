a2518ec518ab448d9e39f8752ab9b7f61a5fa735
Ngoc <ngocbnguyen19@gmail.com>
2022-11-10 13:56:57 -0500

    move folder

--- scripts/search.js
+++ scripts/search.js
@@ -1,8 +1,15 @@
 "use strict";
 
 function loadAllProducts() {
-
-}
+    fetch("http://localhost:8081/api/products")
+        .then(response=>response.json())
+        .then(data=>{
+            data.forEach(p => {
+                const d = document.createElement("div");
+                d.innerHTML = `<a href="detail.html?id=${p.productId}">${p.productName}</a>`;
+                results.append(d);
+})
+        })
 
 window.onload = () => {
     loadAllProducts();
